8795c8fbebce715bea66cdd0e1db94928cdf1d8e
= <=>
2024-03-08 14:58:01 +0300

    add defines methods for set from task

--- src/containers/set/s21_set.h
+++ src/containers/set/s21_set.h
@@ -8,11 +8,37 @@ class set {
  public:
   using key_type = T;
   using value_type = T;
-  using reference = value_type&;
-  using const_reference = const value_type&;
-  //using iterator = SetIterator<T>;
-  //using const_iterator = SetConstIterator<T>;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  using iterator = SetIterator<T>;
+  using const_iterator = SetConstIterator<T>;
   using size_type = size_t;
+
+  set();
+  set(std::initializer_list<value_type> const &items);
+  set(const set &s);
+  set(set &&s);
+  ~set();
+  set &operator=(set &&s);
+  set &operator=(set &s);
+
+  iterator begin();
+  iterator end();
+  const_iterator begin();
+  const_iterator end();
+
+  bool empty();
+  size_type size();
+  size_type max_size();
+
+  void clear();
+  std::pair<iterator, bool> insert(const value_type &value);
+  void erase(iterator pos);
+  void swap(set &other);
+  void merge(set &other);
+
+  iterator find(const_reference &key);
+  bool contains(const_reference &key);
 };
 
 }  // namespace s21
