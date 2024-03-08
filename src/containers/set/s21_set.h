9fec66092c8855dd4aa9696fe2d8764fff483a2b
= <=>
2024-03-08 13:50:58 +0300

    add file for realization set

--- src/containers/set/s21_set.h
+++ src/containers/set/s21_set.h
@@ -6,13 +6,13 @@ namespace s21 {
 template <typename T>
 class set {
  public:
+  using key_type = T;
   using value_type = T;
   using reference = value_type&;
   using const_reference = const value_type&;
-  using iterator = SetIterator<T>;
-  using const_iterator = SetConstIterator<T>;
+  //using iterator = SetIterator<T>;
+  //using const_iterator = SetConstIterator<T>;
   using size_type = size_t;
-
 };
 
 }  // namespace s21
---
+++ src/containers/set/s21_set.tpp
@@ -0,0 +1,12 @@
+#ifndef CPP2_S21_CONTAINERS_1_SRC_CONTAINERS_SET_S21_SET_TPP_
+#define CPP2_S21_CONTAINERS_1_SRC_CONTAINERS_SET_S21_SET_TPP_
+
+#include "s21_set.h"
+
+namespace s21 {
+
+// template <typename T>
+
+}  // namespace s21
+
+#endif  // CPP2_S21_CONTAINERS_1_SRC_CONTAINERS_SET_S21_SET_TPP_
