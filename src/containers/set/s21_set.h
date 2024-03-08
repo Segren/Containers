9b07d19b9dc4f680777f0b36363ee6d21c3c4d76
= <=>
2024-03-08 13:14:42 +0300

    new branch, delete extra folders

--- src/containers/set/s21_set.h
+++ src/containers/set/s21_set.h
@@ -1,4 +1,20 @@
 #ifndef CPP2_S21_CONTAINERS_1_SRC_CONTAINERS_SET_S21_SET_H_
 #define CPP2_S21_CONTAINERS_1_SRC_CONTAINERS_SET_S21_SET_H_
 
+namespace s21 {
+
+template <typename T>
+class set {
+ public:
+  using value_type = T;
+  using reference = value_type&;
+  using const_reference = const value_type&;
+  using iterator = SetIterator<T>;
+  using const_iterator = SetConstIterator<T>;
+  using size_type = size_t;
+
+};
+
+}  // namespace s21
+
 #endif  // CPP2_S21_CONTAINERS_1_SRC_CONTAINERS_SET_S21_SET_H_
