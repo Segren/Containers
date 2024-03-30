0040bceaeaca0923aeed57df26890f6f446a5ea7
= <=>
2024-03-30 14:32:30 +0300

    fix headers order after merging

--- src/s21_containers.h
+++ src/s21_containers.h
@@ -1,10 +1,10 @@
 #ifndef CPP2_S21_CONTAINERS_1_SRC_S21_CONTAINERS_H_
 #define CPP2_S21_CONTAINERS_1_SRC_S21_CONTAINERS_H_
 
-#include "containers/map/s21_map.h"
-#include "containers/set/s21_set.h"
 #include "containers/list/s21_list.h"
+#include "containers/map/s21_map.h"
 #include "containers/queue/s21_queue.h"
+#include "containers/set/s21_set.h"
 #include "containers/stack/s21_stack.h"
 #include "containers/vector/s21_vector.h"
 
